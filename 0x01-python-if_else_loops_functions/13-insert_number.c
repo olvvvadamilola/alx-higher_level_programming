6d54f817044d905ffb0414a8937311bdcda8a0aa
olvvvadamilola <destric3@gmail.com>
2023-11-29 00:36:27 +0100

    Technical interview prep nserts a number into a sorted singly linked list.

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -10,8 +10,7 @@
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
-	listint_t *current = *head;
-	listint_t *prev = NULL;
+	listint_t *current;
 
 	if (new_node == NULL)
 		return (NULL);
@@ -26,14 +25,12 @@ listint_t *insert_node(listint_t **head, int number)
 		return (new_node);
 	}
 
-	while (current && current->n < number)
-	{
-		prev = current;
+	current = *head;
+	while (current->next && current->next->n < number)
 		current = current->next;
-	}
 
-	new_node->next = current;
-	prev->next = new_node;
+	new_node->next = current->next;
+	current->next = new_node;
 
 	return (new_node);
 }
