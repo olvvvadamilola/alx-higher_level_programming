814f41331b0594bbc7f8b8318e6ed07135bd983b
olvvvadamilola <destric3@gmail.com>
2023-11-28 20:58:45 +0100

    program that prints the ASCII alphabet in lowercas

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    print(chr(i), end="")
+for a in range(97, 123):
+    alph = chr(a)
+    print("{}".format(alph), end="")
+
+"""for i in range(ord('a'), ord('z') + 1):
+    print(chr(i), end="")"""
