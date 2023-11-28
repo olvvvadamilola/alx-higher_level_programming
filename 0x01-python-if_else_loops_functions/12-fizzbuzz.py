3c9c2944683498ea53c85a5488b0fc483333a2a6
olvvvadamilola <destric3@gmail.com>
2023-11-29 00:03:59 +0100

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for n in range(1,101):
+    for n in range(1, 101):
         if ((n % 3 == 0) & (n % 5 == 0)):
             print("FizzBuzz", end=" ")
         elif (n % 3 == 0):
