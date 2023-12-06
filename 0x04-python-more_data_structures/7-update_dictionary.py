37579776df13fc4c0166a5beac62c2d8dcba37fb
olvvvadamilola <destric3@gmail.com>
2023-12-06 23:22:30 +0100

    fn that replaces / adds key/value in a dictionary.

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    updated_dictionary = a_dictionary.copy()
-    updated_dictionary[key] = value
-    return updated_dictionary
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
+    return a_dictionary
