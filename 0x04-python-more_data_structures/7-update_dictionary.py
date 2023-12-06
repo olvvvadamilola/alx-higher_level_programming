b767ad4f265ec1a17c19e0d4506f5ccc82043609
olvvvadamilola <destric3@gmail.com>
2023-12-06 23:26:37 +0100

    n that replaces / adds key/value in a dictionary.

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_dictionary = a_dictionary.copy()
-    if key in new_dictionary:
-        new_dictionary[key] = value
+    if key in a_dictionary:
+        a_dictionary[key] = value
     else:
-        new_dictionary[key] = value
-    return new_dictionary
+        a_dictionary[key] = value
+    return a_dictionary
