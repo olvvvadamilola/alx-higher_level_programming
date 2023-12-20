101f01e7fae32a5480f8893eb9a91f74cf76ac94
olvvvadamilola <destric3@gmail.com>
2023-12-20 02:08:16 +0100

    def area(self): that retns the current square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Define square class
+    attributes:
+        size(int): size of the square(private)
+        Return: current square area"""
+
+
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+    size (int): The size of the square.
+
+    Methods:
+    __init__: Initializes a square with an optional size.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a square with the given size.
+
+        Args:
+        size (int, optional): The length of each side of the square.
+        Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is < 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size ** 2
