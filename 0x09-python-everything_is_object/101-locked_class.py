e8e08ba1fe231331e4db5c1bf8a0ad3f188587a9
olvvvadamilola <destric3@gmail.com>
2024-01-05 12:18:26 +0100

    check

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,10 +5,8 @@
 class LockedClass:
     """override the __setattr__ method to control attribute assignment"""
     def __setattr__(self, name, value):
-        """if the attribute name is first_name, allow assignment"""
         if name == "first_name":
-            """use the super() function to access the parent class method"""
             super().__setattr__(name, value)
         else:
-            raise AttributeError("'LockedClass' object has no attribute '{}'"
-                                 format(name))
+            msg = "'LockedClass' object has no attribute '{}'"
+            raise AttributeError(msg.format(name))
