78952172e05963add73058709bd6dc3956863fe2
olvvvadamilola <destric3@gmail.com>
2024-01-05 12:41:24 +0100

    check

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,10 +3,12 @@
 
 
 class LockedClass:
-    """override the __setattr__ method to control attribute assignment"""
+    """define a locked class"""
+    __slots__ = ['first_name']
+
     def __setattr__(self, name, value):
-        if name == "first_name":
-            super().__setattr__(name, value)
-        else:
-            msg = "'LockedClass' object has no attribute '{}'"
-            raise AttributeError(msg.format(name))
+        if name != 'first_name':
+            raise AttributeError(
+                "'LockedClass' object has no attribute '{}'".format(name)
+            )
+        super().__setattr__(name, value)
