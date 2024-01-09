56d7d857abf6cda0c54d6fca99ade30b19220b0b
olvvvadamilola <destric3@gmail.com>
2024-01-09 16:42:01 +0100

    Write a class Student that defines a student by: (based on 9-student.py)

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a class Student"""
+
+
+class Student:
+    """Class defining a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize student attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return dictionary representation of the instance"""
+        if attrs is None:
+            return self.__dict__
+
+        return {attr: getattr(self, attr)
+                for attr in attrs if hasattr(self, attr)}
