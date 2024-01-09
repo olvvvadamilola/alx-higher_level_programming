cd8227c916e46c3aa774901860800c64e2fd13f7
olvvvadamilola <destric3@gmail.com>
2024-01-09 15:41:28 +0100

    returns an object (Python data structure) represented by a JSON string:

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """returns an object (Python data structure)"""
 
+
 def from_json_string(my_str):
     """returns an object (Python data structure)
     represented by a JSON string"""
