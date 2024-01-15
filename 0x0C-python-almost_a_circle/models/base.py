04c2b17c9fe1bbb46dae233701ee9a9d644ad7ab
olvvvadamilola <destric3@gmail.com>
2024-01-15 15:05:23 +0100

    updated base, rectangle and square

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Base class for all models, managing object IDs"""
+import json
+import csv
 
 
 class Base:
@@ -19,3 +21,58 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of list_dictionaries."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON string representation of list_objs to a file."""
+        filename = "{}.json".format(cls.__name__)
+        json_list = []
+        if list_objs is not None:
+            json_list = [obj.to_dictionary() for obj in list_objs]
+
+        with open(filename, 'w') as file:
+            file.write(cls.to_json_string(json_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the list of the JSON string representation json_string."""
+        if json_string is None or json_string == '':
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return an instance with all attributes already set."""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        else:
+            dummy = None
+
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of instances."""
+        filename = cls.__name__ + ".json"
+
+        try:
+            with open(filename, 'r') as file:
+                data = file.read()
+                if data:
+                    dictionaries = cls.from_json_string(data)
+                    instances = [cls.create(**dic) for dic in dictionaries]
+                    return instances
+                else:
+                    return []
+        except FileNotFoundError:
+            return []
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square that inherits from Rectangle."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """constructor for Square.
+
+        Args:
+        size (int): size of square
+        x (int): x coordinate of square. Defaults to 0.
+        y (int): y coordinate of square. Defaults to 0.
+        id (int): id of square. Defaults to None.
+        """
+
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """string representation of Square.
+
+        Returns:
+        str: string representation of Square"""
+
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """size setter"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Update attributes of the Square."""
+        attributes = ['id', 'size', 'x', 'y']
+        if args:
+            for i in range(len(args)):
+                setattr(self, attributes[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Return the dictionary representation of a Square."""
+        return {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+        }
